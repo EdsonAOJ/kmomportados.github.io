1493cd8e4ebd781ff518f996fa2dbdcdb61e3eaa
Edson <edsonaoj@gmail.com>
2021-08-28 14:05:07 -0300

    [teste]

---
+++ src/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
+
+
+const server = express();
+
+
+server
+  .use(express.static('public'))
+
+  //ultilizar o body da require
+  .use(express.urlencoded({ extended: true }))
+
+  //configurar tamplete engine
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+
+  // rotas da aplicação
+  .get('/', (req, res) => {
+    res.render('index');
+  })
+
+
+
+
+  const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log('servidor rodando');
+});
